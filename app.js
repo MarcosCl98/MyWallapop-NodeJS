4d2e01f2c9391288e366f8e4dd6feaeaeb2da7f6
MarcosCl98 <marcoscl7298@gmail.com>
2019-04-11 03:17:18 +0200

    Features: creado proyecto base, con las carpetas creadas y el pakage json con las dependencias necesarias iniciales.

---
+++ app.js
@@ -0,0 +1,25 @@
+// Módulos
+var express = require('express');
+var app = express();
+
+var swig = require('swig');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Carpeta publica
+app.use(express.static('public'));
+
+// Variables
+app.set('port', 8081);
+
+
+//Rutas/controladores por lógica
+//require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
+
+
+// lanzar el servidor
+app.listen(app.get('port'), function() {
+    console.log("Servidor activo");
+})
