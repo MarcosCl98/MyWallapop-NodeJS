ad8122a13641ed466646f5cc66b39d3f24567af4
Martin Fernandez Prieto <tatin1998@gmail.com>
2019-04-13 13:57:29 +0200

    Metido el favicon, ademas ya funcionando vista de añadir, voya hacer merge esperando a que se acabe lo de los usuarios

--- routes/rbids.js
+++ routes/rbids.js
@@ -1,3 +1,7 @@
 module.exports = function (app, swig, bidsRepository) {
-
+    //Añadir una nueva oferta
+    app.get("/bid/add", function (req, res) {
+        var respuesta = swig.renderFile('views/bids/add.html');
+        res.send(respuesta);
+    });
 }
