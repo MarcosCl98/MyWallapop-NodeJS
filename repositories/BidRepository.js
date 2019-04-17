4bb8f1ea10a1d7de379680080242d53d441b849c
Martin Fernandez Prieto <tatin1998@gmail.com>
2019-04-17 02:24:01 +0200

    fix: borrada una funcion que sobraba

--- repositories/BidRepository.js
+++ repositories/BidRepository.js
@@ -79,23 +79,6 @@ module.exports = {
      * @param criterio
      * @param funcionCallback
      */
-    removeBidByEmail: function (criterio, funcionCallback) {
-        this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
-            if (err) {
-                funcionCallback(null);
-            } else {
-                var collection = db.collection('bids');
-                collection.remove({'userEmail': {'$in': criterio}}, function (err, result) {
-                    if (err) {
-                        funcionCallback(null);
-                    } else {
-                        funcionCallback(result);
-                    }
-                    db.close();
-                });
-            }
-        });
-    },
     removeBid: function (criterio, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
             if (err) {
